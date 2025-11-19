d8fe316880b58fb5460c82b35fbd79a94c76021b
MURGEY Sebastien <sebastien.murgey@rte-france.com>
2025-11-19 18:07:37 +0100

    Try fixing issue

--- src/helpers/PagesHelper.ts
+++ src/helpers/PagesHelper.ts
@@ -303,7 +303,7 @@ export class PagesHelper {
       // Add new markdown web part
       await execScript(
         ArgumentsHelper.parse(
-          `spo page clientsidewebpart add --webUrl "${webUrl}" --pageName "${slug}" --webPartId 1ef5ed11-ce7b-44be-bc5e-4abd55101d16 --webPartData @${wpData} --verticalSection`
+          `spo page clientsidewebpart add --webUrl "${webUrl}" --pageName "${slug}" --webPartId 1ef5ed11-ce7b-44be-bc5e-4abd55101d16 --webPartData @${wpData} --column 1`
         ),
         CliCommand.getRetry()
       );
